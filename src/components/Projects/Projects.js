9d943f0c4f3f9ac23f9e167c82786bac3e11cba4
Reza <reza.bozorgi@outlook.com>
2021-09-12 16:38:57 +0430

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map((project) => {
+        return (
+          <BlogCard id={project.id}>
+            <Img src={project.image} />
+            <TitleContent>
+              <HeaderThree title>{project.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{project.description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {project.tags.map((tag, i) => {
+                  return <Tag key={i}>{tag}</Tag>;
+                })}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={project.visit}>Code</ExternalLinks>
+              <ExternalLinks href={project.visit}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        );
+      })}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "Freight App",
+    description:
+      "Using React, Typescript and Mobx with .Net Core as back-end. this is a functional application for logistic department and their customers to intative withou a single call",
+    image: "/images/projects.jpeg",
+    tags: ["React", "Typescript", ".Net Core", "EF Core"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "CRM vLight",
+    description:
+      "Using React, Typescript and Mobx with .Net Core as back-end. this is a functional application for logistic department and their customers to intative withou a single call",
+    image: "/images/projects.jpeg",
+    tags: ["React", "JavaScript"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "Foo App",
+    description:
+      "Using React, Typescript and Mobx with .Net Core as back-end. this is a functional application for logistic department and their customers to intative withou a single call",
+    image: "/images/projects.jpeg",
+    tags: ["React", "Typescript", "Tailwind"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Coin Market",
+    description:
+      "Using React, Typescript and Mobx with .Net Core as back-end. this is a functional application for logistic department and their customers to intative withou a single call",
+    image: "/images/projects.jpeg",
+    tags: ["React", "Typescript", "App4Dev"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2012, text: "" },
+  { year: 2015, text: "" },
+  { year: 2019, text: "" },
+  { year: 2020, text: "" },
+  { year: 2021, text: "" },
+];
