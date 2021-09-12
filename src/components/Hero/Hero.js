bc296f5f70bfd8a2560d109e3cb15ad111197b71
Reza <reza.bozorgi@outlook.com>
2021-09-12 12:29:20 +0430

    Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a passionate software engineer who is focusing on Front-end
+        development with React, Typescript, and related technologies. I’m able
+        to analyze customer needs and designing technical solutions. I have
+        enthusiastic to apply best software practices and writing reliable and
+        maintainable code.
+      </SectionText>
+      <Button>Contact me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
